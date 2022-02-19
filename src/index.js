30e1ebf1857be0c0415aeee9d68c6c5e74ef5eac
Hassan Alsabeh <72017216+HassanAlsabeh@users.noreply.github.com>
2022-02-19 22:02:39 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
 
+import reportWebVitals from "./reportWebVitals";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import "bootstrap/dist/js/bootstrap.js";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import 'flag-icon-css/css/flag-icon.min.css'
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ["en", "fr", "ar"],
+    fallbackLng: "en",
+    detection: {
+      order: ["path"],
+      caches: ["cookie"],
+    },
+    backend: {
+      loadPath: "/assets/locales/{{lng}}/translation.json",
+    },
+  });
+
+// function App() {
+//   const { t } = useTranslation();
+
+//   return <h2>{t("welcome_to_react")}</h2>;
+// }
+
+const loadingMarkup = (
+  <div className="py-4 text-center">
+    <h3>Loading..</h3>
+  </div>
+);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
+
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
